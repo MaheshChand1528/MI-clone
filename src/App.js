c34f29c92066233f48304b2ea37c42f0815ab849
MaheshChand1528 <mannu44800@gmail.com>
2022-07-13 16:39:54 +0530

    builded mi clone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import Heading from "./components/Heading";
+import HotAccessoriesMenu from "./components/HotAccessoriesMenu ";
+import Navbar from "./components/Navbar";
+import Offers from "./components/Offers";
+import Postnavbar from "./components/Postnavbar";
+import ProductReviews from "./components/ProductReviews";
+import SliderImg from "./components/SliderImg";
+import StarProducts from "./components/StarProducts";
+import Videos from "./components/Videos";
+import data from "./Data/data.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Postnavbar
+        miPhones={data.miPhones}
+        redmiPhones={data.redmiPhones}
+        tv={data.tv}
+        laptop={data.laptop}
+        fitnessAndLifeStyle={data.fitnessAndLifeStyle}
+        home={data.home}
+        audio={data.audio}
+        accessories={data.accessories}
+      />
+      <Banner start={data.banner.start} />
+      <Offers offer={data.offer} />
+      <Heading heading="STAR PRODUCTS" />
+      <StarProducts starProduct={data.starProduct} />
+      <Heading heading="HOT ACCESSORIES" />
+      <HotAccessoriesMenu />
+      <Heading heading="PRODUCT REVIEWS" />
+      <ProductReviews productReviews={data.productReviews} />
+      <Heading heading="VIDEOS" />
+      <Videos videos={data.videos} />
+      <Heading heading="IN THE PRESS" />
+      <SliderImg end={data.banner.end} />
+      <Footer footer={data.footer}/>
+    </>
   );
 }
 
